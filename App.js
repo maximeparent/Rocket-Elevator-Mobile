8b428aa3f6c06b46e6adacf82a8346e836c4bdb2
MaX Parent <maxparent7@gmail.com>
2020-05-08 12:14:23 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Login</Text>
+      <TextInput style={styles.input} 
+      placeholder= "Email" 
+      />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+        style={styles.userBtn}
+        onPress= {() => alert("Welcome")}
+        >
+          <Text style= {styles.btnTxt}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1e90ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign:'center',
+    margin:10,
+    color: "#fff",
+    fontFamily: "Roboto"
+  },
+  input:{
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10
+  },
+  btnContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  userBtn: {
+    backgroundColor: "#FFD700",
+    padding: 15,
+    width: "45%"
+  },
+  btnTxt: {
+    fontSize: 18,
+    textAlign: "center"
+  }
+});
